c11d4ee61eb1ec2593be2c30fbf2d26b935a01b0
David <dav.llwyr@gmail.com>
2021-06-23 12:30:53 -0600

    add folders for character, beast, heroe, executable

---
+++ src/characters/Character.java
@@ -0,0 +1,12 @@
+public class Character {
+    private int life;
+    private int attack;
+    public Character(int life, int attack){
+        this.life = life;
+        this.attack = attack;
+    }
+
+    public void offensivePower(){
+        //ok
+    }
+}
---
+++ src/executable/Main.java
@@ -0,0 +1,11 @@
+package executable;
+
+public class Main {
+    public static void main(String[] args){
+
+        Main m = new Main();
+    }
+    public Main() {
+        
+    }
+}
